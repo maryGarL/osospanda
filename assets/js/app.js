855a8c12f0d8a10b9a5004da0504c84d6ed0d2bc
Mariel Garcia <maryel_565@hotmail.com>
2017-04-17 21:47:30 -0500

    terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 var contenedor=document.getElementById("contenedor");
-
+var restaurar=document.getElementById("mostrar");
+var mostrar=document.getElementById("etiqueta1");
+var borrar=document.getElementById("etiqueta2");
 for (var i = 0; i <= 3; i++) {
 var div=document.createElement("div");
 var image=document.createElement("img");
@@ -12,3 +14,22 @@ div.appendChild(image);
 div.appendChild(span);
 contenedor.appendChild(div);
 }
+
+restaurar.setAttribute("onclick","onClick()")
+function onClick() {
+  for (var i = 0; i <= 3; i++){
+    document.getElementsByClassName('caja-foto')[i].style.display ='inline-block';
+  }
+  }
+
+borrar.setAttribute("onclick","quitar()")
+function quitar() {
+    document.getElementById('mostrarTexto').style.display ='none';
+
+  }
+
+  mostrar.setAttribute("onclick","mostrar1()")
+  function mostrar1() {
+      document.getElementById('mostrarTexto').style.display ='inline-block';
+
+    }
